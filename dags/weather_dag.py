cf977c3a2fd5eedec9cef63e3e2ce9b132307fee
morn1n9st4r <a.lepilo.soft@gmail.com>
2023-03-16 20:42:03 +0200

    kyiv weather data from json

--- dags/weather_dag.py
+++ dags/weather_dag.py
@@ -14,6 +14,7 @@ from airflow.models import Variable
 
 lat = 50.4501
 lng = 30.5234
+ua_timezone = 2
 
 # airflow variable
 wwo_key = Variable.get("KEY_API_WWO")
@@ -24,11 +25,12 @@ def extract_data_kyiv(**kwargs):
     response = requests.get(
             'http://api.worldweatheronline.com/premium/v1/weather.ashx',
             params={
-                'q':'{},{}'.format(lat,lng),
-                'tp':'1',
+                'q':'Kiev',
+                'tp':'24',
                 'num_of_days':1,
                 'format':'json',
-                'key':wwo_key
+                'key':wwo_key,
+                'showlocaltime':'yes'
             },
             headers={
                 'Authorization': wwo_key
@@ -41,11 +43,39 @@ def extract_data_kyiv(**kwargs):
 
         ti.xcom_push(key='wwo_kyiv_json', value=json_data)
 
-def transform_data():
-    pass
 
-def load_data():
-    pass
+
+def transform_data_kyiv(**kwargs):
+    ti = kwargs['ti']
+    json_data = ti.xcom_pull(key='wwo_kyiv_json', task_ids=['extract_data_kyiv'])[0]
+    request_data = json_data['data']['request']
+    time_data = json_data['data']['time_zone']
+    weather_data = json_data['data']['current_condition']
+# 2023-03-16 20:11
+    timestamp = datetime.datetime.strptime(time_data[0]['localtime'], '%Y-%m-%d %H:%M')
+    """ 
+    print(timestamp)
+    date = timestamp.date()
+    print(date)
+    time = timestamp.time()
+    print(time)
+     """
+    js = {
+        'date': str(timestamp.date()),
+        'time': str(timestamp.time()),
+        'city': request_data[0]['query'],
+        'weather': weather_data[0]['weatherDesc'][0]['value'],
+        'temp_C': weather_data[0]['temp_C'],
+        'temp_C_feels_like': weather_data[0]['FeelsLikeC'],
+        'wind_speed_kmph': weather_data[0]['windspeedKmph'],
+        'wind_from': weather_data[0]['winddir16Point'],
+        'humidity': weather_data[0]['humidity'],
+        'pressure': weather_data[0]['pressure'],
+        'cloudcover': weather_data[0]['cloudcover'],
+        
+    }
+
+    ti.xcom_push(key='weather_kyiv_json_filtered', value=js)
 
 
 
@@ -63,6 +93,9 @@ with DAG('load_weater_data',
          schedule='@daily',
          default_args=args
         ) as dag:
+
     extract_data = PythonOperator(task_id='extract_data_kyiv', python_callable=extract_data_kyiv)
+
+    transform_data  = PythonOperator(task_id='transform_data_kyiv', python_callable=transform_data_kyiv)
     
-    extract_data
+    extract_data >> transform_data
